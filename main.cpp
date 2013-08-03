d70265e624beca5a5820d9e479bc1f2c88d204f6
ollegator <ollegator@home.(none)>
2013-08-04 01:01:16 +0300

    1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 
-    // yui
+    // 5
 
     char fullpath[500];                                                     // путь к приложению...
     int length = readlink("/proc/self/exe", fullpath, sizeof(fullpath));
